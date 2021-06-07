9390e476f45f500eaa7d9ce6758e3b15d8b9b3c9
xrotox <mohammedghaouti@gmail.com>
2021-06-07 15:39:07 +0100

    products added

--- src/main/java/sud/domain/Product.java
+++ src/main/java/sud/domain/Product.java
@@ -1,5 +1,48 @@
 package sud.domain;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "product")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Product {
 
+	private Long id;
+	private String label;
+	private double price;
+	
+	public Product() {}
+	
+	public Product(Long id, String label, double price) {
+		this.id = id;
+		this.label = label;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
--- src/main/java/sud/domain/Products.java
+++ src/main/java/sud/domain/Products.java
@@ -1,5 +1,56 @@
 package sud.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.sun.xml.txw2.annotation.XmlElement;
+
+@XmlRootElement(name = "products")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Products {
+	
+	@XmlElement(name = "product") 
+	private List<Product> products;
+	
+	public Products() {
+		products = new ArrayList<Product>();
+	}
+	
+	public Products(List<Product> products) {
+		this.products = products;
+	}
+	
+	public long add(Product product) {
+		products.add(product);
+	}
+	
+	public Product get(long id) {
+		for(Product product: products) {
+			if(id == product.getId()) {
+				return product;
+			}
+		}
+		return null;
+	}
+	
+	public boolean remove(long id) {
+		for(Product product: products) {
+			if(id == product.getId()) {
+				int index = products.indexOf(product);
+				products.remove(index);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean removeAll() {
+		products = new ArrayList<Product>();
+	}
+	
 
 }
