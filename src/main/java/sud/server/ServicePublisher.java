2798bcde603d8baeef340e8dc9643a20c51f441a
Mohammed Rhaouti <31079981+XRotoX@users.noreply.github.com>
2021-06-13 01:10:07 +0100

    Create ServicePublisher.java

--- src/main/java/sud/server/ServicePublisher.java
+++ src/main/java/sud/server/ServicePublisher.java
@@ -13,11 +13,7 @@ public class ServicePublisher {
 	public static void main(String[] args) {
 		
 		logger.info("Starting service");
-<<<<<<< HEAD
 		Endpoint.publish("http://localhost:2021/webservices/PoductsMAnager", new ProductsManagerImpl());
-=======
-		Endpoint.publish("http://localhost:2020/webservices/ProductsManager", new ProductsManagerImpl());
->>>>>>> 0e336e677af753603c9c5dc28bec5c0a91209612
 		logger.info("Server started");
 	}
 }
